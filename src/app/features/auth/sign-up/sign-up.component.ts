bea488e91c19ef435b68615ac8ec23e6dc1d00b2
gurai <gura.ilya2011@yandex.ru>
2024-11-09 14:19:37 +0300

    improve auth structure

--- src/app/features/auth/sign-up/sign-up.component.ts
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {DividerModule} from "primeng/divider";
+import {InputTextModule} from "primeng/inputtext";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [DividerModule, ButtonModule, InputTextModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
