44fbee6db059dba085ab741c2ccdff304e5087a0
gurai <gura.ilya2011@yandex.ru>
2024-11-09 12:34:28 +0300

    improve auth structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,34 @@ import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'sign-in', loadComponent: () =>
-      import('./pages/auth/sign-in-page/sign-in-page.component').then(
-        (m) => m.SignInPageComponent
-      ),
-  },
-  {
-    path: 'sign-up', loadComponent: () =>
-      import('./pages/auth/sign-up-page/sign-up-page.component').then(
-        (m) => m.SignUpPageComponent
-      )
-  },
-  {
     path: 'home', loadComponent: () =>
       import('./pages/home-page/home-page.component').then(
         (m) => m.HomePageComponent
       ),
     canActivate: []
   },
-  { path: '**', redirectTo: 'sign-in' }
+  {
+    path: 'auth',
+    loadComponent: () => import('./pages/auth-layout/auth-layout.component').then((m) => m.AuthLayoutComponent),
+    children: [
+      {
+        path: '', // Пустой путь для редиректа при попытке доступа к /auth
+        redirectTo: 'sign-in',
+        pathMatch: 'full'
+      },
+      {
+        path: 'sign-in', loadComponent: () =>
+          import('./widgets/auth/sign-in-page/sign-in-page.component').then(
+            (m) => m.SignInPageComponent
+          ),
+      },
+      {
+        path: 'sign-up', loadComponent: () =>
+          import('./widgets/auth/sign-up-page/sign-up-page.component').then(
+            (m) => m.SignUpPageComponent
+          )
+      },
+    ]
+  },
+  {path: '**', redirectTo: 'auth', pathMatch: 'full'}
 ];
