19f5d3dbc234d9f8cadb0b4b90a97c2f44aab240
Ilya <gura.ilya2011@yandex.ru>
2025-02-14 17:42:00 +0300

    Добавлен компонент для создания постов

---
+++ src/app/widgets/post-form/post-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BaseInputComponent } from '../../shared/ui/base-input/base-input.component';
+import { BaseButtonComponent } from '../../shared/ui/base-button/base-button.component';
+import { ButtonBackgroundColors } from '../../shared/ui/base-button/button-background-colors';
+
+@Component({
+  selector: 'app-post-form',
+  standalone: true,
+  imports: [
+    BaseInputComponent,
+    ReactiveFormsModule,
+    BaseButtonComponent,
+  ],
+  templateUrl: './post-form.component.html',
+  styleUrl: './post-form.component.scss',
+})
+export class PostFormComponent {
+
+  private readonly fb = inject(FormBuilder);
+
+  public readonly formGroup = this.fb.group({
+    caption: ['', Validators.required],
+    mediaData: [null],
+    location: [''],
+    tags: [''],
+    altText: [''],
+  });
+
+  public readonly noneColor = ButtonBackgroundColors.none;
+
+  public onSubmitForm(): void {
+
+  }
+}
